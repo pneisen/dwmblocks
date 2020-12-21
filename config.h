34aef7b3001544f0748245868bd06712e4742130
Pete Neisen <pete@neisen.xyz>
2020-12-21 09:55:44 -0700

    change the config

--- config.h
+++ config.h
@@ -2,10 +2,10 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	/* {"⌨", "kbselect", 0, 30}, */
-	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
+	/* {"", "cat /tmp/recordingicon 2>/dev/null",	0,	9}, */
 	/* {"",	"music",	0,	11}, */
-	{"",	"pacpackages",	0,	8},
-	{"",	"news",		0,	6},
+	/* {"",	"pacpackages",	0,	8}, */
+	/* {"",	"news",		0,	6}, */
 	/* {"",	"crypto",	0,	13}, */
 	/* {"",	"price lbc \"LBRY Token\" 📚",			9000,	22}, */
 	/* {"",	"price bat \"Basic Attention Token\" 🦁",	9000,	20}, */
@@ -17,14 +17,16 @@ static const Block blocks[] = {
 	/* {"",	"memory",	10,	14}, */
 	/* {"",	"cpu",		10,	18}, */
 	/* {"",	"moonphase",	18000,	17}, */
-	{"",	"forecast",	18000,	5},
+	/* {"",	"forecast",	18000,	5},
 	{"",	"mailbox",	180,	12},
 	{"",	"nettraf",	1,	16},
 	{"",	"volume",	0,	10},
-	{"",	"battery",	5,	3},
-	{"",	"clock",	60,	1},
+	{"",	"battery",	5,	3}, */
+	{"",	"clock",	30,	1},
+  /*
 	{"",	"internet",	5,	4},
 	{"",	"help-icon",	0,	15},
+  */
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
